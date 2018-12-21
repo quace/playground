d0871e87c9633e0e7ad2409a70bdbed2e3c31d8a
quace <quinten.moesen@gmail.com>
2018-12-21 19:08:40 +0100

    First webapp cleaned

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
+
+func main() {
+
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
+
+}
